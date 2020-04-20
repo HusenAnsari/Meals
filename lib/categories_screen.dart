b3cbc3fb65a5918b964527534aed4cb6e6049b52
Husen Ansari <husenansari07@gmail.com>
2020-04-20 13:00:43 +0530

    Project Setup and Categories Screen

---
+++ lib/categories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:mealsapp/category_item.dart';
+import 'package:mealsapp/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (category) => CategoryItem(
+                category.title,
+                category.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
